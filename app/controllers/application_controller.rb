d65608e443ac6ad9d419c083b14d98e486650dc5
Melissa Parker <mmp368@nyu.edu>
2017-06-22 19:26:48 +0000

    help

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,16 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
+  post '/rossified' do
+    @url = params["link"]
+    erb :results
+  end
+  
+  post '/bobgif' do
+   search_term = params["gif"]
+   @gifurl = get_gif(search_term)
+   erb :gif
+  end
+  
 end
--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,10 @@
-class SampleModel
-  attr_accessor :name, :age
-end
+require 'json'
+require 'net/http'
+ 
+ def get_gif(search_term)
+    request_string="http://api.giphy.com/v1/gifs/random?&tag=#{search_term.gsub(" ","+")+"+drawing"}&rating=g&api_key=dc6zaTOxFJmzC"
+    sample_uri=URI(request_string)
+    sample_response=Net::HTTP.get(sample_uri)
+    sample_parsed_response=JSON.parse(sample_response)
+    return sample_parsed_response["data"]["image_url"]
+ end
